167306e27fbb0c128562bdb282d81d5451c57a38
Sanjay Rai <sanjaywryee@gmail.com>
2023-05-29 16:06:59 +0545

    second

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const dotenv = require('dotenv').config();
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT,(req, res) => {
-    console.log(`server started  on port ${PORT}/`);
+    console.log(`server running on port ${PORT}`);
 });
